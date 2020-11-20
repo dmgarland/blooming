f5d673ff7cd9186169f7bdfd01b9ee65447039fd
Dan Garland <dan@dangarland.co.uk>
2020-11-20 13:07:00 +0000

    using the mouse position on the viewport to control pitch

--- Main.gd
+++ Main.gd
@@ -5,6 +5,7 @@ var movementOffset = 0.2
 var cameraAngle = 0
 var maxNotes = 12
 var notes = 0
+var basePitch = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,11 +14,11 @@ func _ready():
 
 func playNote(index):
 	notes += 1
-	var pitch = r.randf_range(1.0, 3.0)
+	var randPitch = r.randf_range(-2.0, 2.0)
 	
 	var note = Note.instance()
 	note.r = r
-	note.pitch = pitch
+	note.pitch = basePitch + randPitch
 	note.index = index
 	#add_child(note)
 	call_deferred("add_child", note)
@@ -29,9 +30,11 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		var offset = -deg2rad(event.relative.y) * 0.5
 		var newAngle = cameraAngle + offset
-		if newAngle > -50 and newAngle < 50:
-			cameraAngle += offset	
+		if newAngle > -60 and newAngle < 60:
+			cameraAngle += offset			
 			$World/Camera.rotate_x(deg2rad(offset))
+		basePitch = (((get_viewport().size.y - get_viewport().get_mouse_position().y) / get_viewport().size.y) - 0.5) * 15		
+		
 	if event is InputEventKey and event.pressed:
 		match event.scancode:
 			KEY_LEFT	:				
