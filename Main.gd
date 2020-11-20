2946d06f92402019c8b242f512e4297cbeab6e4b
Dan Garland <dan@dangarland.co.uk>
2020-11-20 12:31:58 +0000

    Limiting maximum notes to prevent accidentally crashing my browser a lot

--- Main.gd
+++ Main.gd
@@ -3,6 +3,8 @@ export (PackedScene) var Note
 var r = RandomNumberGenerator.new()
 var movementOffset = 0.2
 var cameraAngle = 0
+var maxNotes = 12
+var notes = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,6 +12,7 @@ func _ready():
 
 
 func playNote(index):
+	notes += 1
 	var pitch = r.randf_range(1.0, 3.0)
 	
 	var note = Note.instance()
@@ -20,8 +23,9 @@ func playNote(index):
 	call_deferred("add_child", note)
 
 func _input(event):
-	if event is InputEventMouseButton && event.is_pressed():
+	if event is InputEventMouseButton && event.is_pressed() && notes < maxNotes:
 		playNote(event.button_index)
+		
 	if event is InputEventMouseMotion:
 		var offset = -deg2rad(event.relative.y) * 0.5
 		var newAngle = cameraAngle + offset
