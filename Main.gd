65358a4aca1ac049703e21a5d69e5d485b285e98
Dan Garland <dan@dangarland.co.uk>
2020-11-22 15:35:06 +0000

    works on the oculus quest

--- Main.gd
+++ Main.gd
@@ -9,7 +9,8 @@ var basePitch = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	r.randomize()
+	r.randomize()		
+	vr.initialize()
 
 
 func playNote(index):
@@ -23,27 +24,6 @@ func playNote(index):
 	#add_child(note)
 	call_deferred("add_child", note)
 
-func _input(event):
-	if event is InputEventMouseButton && event.is_pressed() && notes < maxNotes:
-		playNote(event.button_index)
-		
-	if event is InputEventMouseMotion:
-		var offset = -deg2rad(event.relative.y) * 0.5
-		var newAngle = cameraAngle + offset
-		if newAngle > -60 and newAngle < 60:
-			cameraAngle += offset			
-			$World/Camera.rotate_x(deg2rad(offset))
-		basePitch = (((get_viewport().size.y - get_viewport().get_mouse_position().y) / get_viewport().size.y) - 0.5) * 10
-		
-	if event is InputEventKey and event.pressed:
-		match event.scancode:
-			KEY_LEFT	:				
-				$World/Camera.rotate_y(0.1)
-			KEY_RIGHT:				
-				$World/Camera.rotate_y(-0.1)
-			KEY_UP:				
-				$World/Camera.translate(Vector3(0, 0, -movementOffset))
-			KEY_DOWN:				
-				$World/Camera.translate(Vector3(0, 0, movementOffset))
-		
-
+func _physics_process(delta):
+	if vr.button_just_pressed(vr.BUTTON.ENTER) && notes < maxNotes:
+		playNote(1)
