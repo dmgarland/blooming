58a5d79ef73390ad5d69e98901dd40f4bf1e2ca4
Dan Garland <dan@dangarland.co.uk>
2020-11-30 16:55:11 +0000

    Adding spiral

--- Main.gd
+++ Main.gd
@@ -11,6 +11,7 @@ var basePitch = 0
 func _ready():
 	r.randomize()		
 	vr.initialize()
+	
 
 
 func playNote(index):
@@ -25,5 +26,30 @@ func playNote(index):
 	call_deferred("add_child", note)
 
 func _physics_process(delta):
-	if vr.button_just_pressed(vr.BUTTON.ENTER) && notes < maxNotes:
-		playNote(1)
+	var x1 = $OQ_ARVROrigin/OQ_LeftController.transform.origin.x
+	var y1 = $OQ_ARVROrigin/OQ_LeftController.transform.origin.y
+	var z1 = $OQ_ARVROrigin/OQ_LeftController.transform.origin.z
+	var t1 = atan2(z1, x1)
+	var p1 = t1 + 1
+
+	var x2 = $OQ_ARVROrigin/OQ_RightController.transform.origin.x
+	var y2 = $OQ_ARVROrigin/OQ_RightController.transform.origin.y
+	var z2 = $OQ_ARVROrigin/OQ_RightController.transform.origin.z
+	var t2 = atan2(z2, x2)
+	var p2 = t2 + 1
+	
+	if p1 > 0:	
+		$OQ_ARVROrigin/OQ_LeftController/LeftAudio.pitch_scale = p1
+	if p2 > 0:
+		$OQ_ARVROrigin/OQ_RightController/RightAudio.pitch_scale = p2
+	
+	if vr.button_just_pressed(vr.BUTTON.LEFT_INDEX_TRIGGER):				
+		print("x = ", x1, " y = ", y1, " z = ", z1, " t = ", t1, " p1 = ", p1)
+		$OQ_ARVROrigin/OQ_LeftController/LeftAudio.play()
+	elif vr.button_just_released(vr.BUTTON.LEFT_INDEX_TRIGGER):		
+		$OQ_ARVROrigin/OQ_LeftController/LeftAudio.stop()
+	elif vr.button_just_pressed(vr.BUTTON.RIGHT_INDEX_TRIGGER):			
+		print("x = ", x2, " y = ", y2, " z = ", z2, " t = ", t2, " p1 = ", p2)
+		$OQ_ARVROrigin/OQ_RightController/RightAudio.play()
+	elif vr.button_just_released(vr.BUTTON.RIGHT_INDEX_TRIGGER):		
+		$OQ_ARVROrigin/OQ_RightController/RightAudio.stop()
